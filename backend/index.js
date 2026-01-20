f60ceca07d2f50b8f863e6775bc3e80d501980f5
sashasutton <sashasuttons3@gmail.com>
2026-01-20 14:58:58 +0100

    Add CORS middleware to backend and implement Navbar component in frontend.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors'
+
 
 import archiveRoutes from './src/routes/archive.route.js';
 
@@ -7,6 +9,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/health', (req, res) => {
     res.json({ status: "ok", message: "Server is running"});
